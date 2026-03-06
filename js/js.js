087ce7eae1fb1dbc3bf0f5d9f1717bbfc3dd56ab
nan <chanyanuch.sit@gmail.com>
2026-03-07 02:14:28 +0700

    update

--- js/js.js
+++ js/js.js
@@ -14,4 +14,33 @@ $(document).ready(function() {
         location.reload();
     });
 
+    $('.link-currency').click(function(){
+        $(this).next('.menu-dropdown').toggleClass('show');
+    });
+
+
+	$(window).on("scroll", function () {
+		var scrollTop = $(this).scrollTop();
+		var windowH = $(window).height();
+
+		$(".sc-new .image, .sc-bestseller .image").each(function () {
+			var $image = $(this);
+			var $text = $image.find("figcaption");
+
+			var imageTop = $image.offset().top;
+			var imageH = $image.outerHeight();
+			var textH = $text.outerHeight();
+
+			var trigger = imageTop - windowH / 2 + imageH / 2;
+			var move = (scrollTop - trigger) * 0.5;
+			var maxMove = (imageH / 2) - (textH / 2);
+			move = Math.max(0, Math.min(move, maxMove));
+
+			$text.css(
+				"transform",
+				"translate(-50%, calc(-50% + " + move + "px))"
+			);
+		});
+	});
+
 });
